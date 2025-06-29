5ccf2856d83d980597bfb2e54f4cf6f898b0dbf0
bintreid4 <bintreid4@gmail.com>
2025-06-29 23:57:09 +0300

    Initial commit for Jambo bot

---
+++ main.py
@@ -0,0 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я бот Jambo 🤖")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
